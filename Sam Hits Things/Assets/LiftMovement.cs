e2457d47b7298aaf85642380df91a781e8d2fae8
Harry <harryrince@gmail.com>
2021-04-05 18:51:07 +0100

    Lift fix

--- Sam Hits Things/Assets/LiftMovement.cs
+++ Sam Hits Things/Assets/LiftMovement.cs
@@ -6,6 +6,7 @@ public class LiftMovement : MonoBehaviour
 {
     [SerializeField] float maximumDescent;
 
+    private bool liftLeft;
     private Vector3 startPos;
     private Vector3 targetPos;
 
@@ -14,18 +15,26 @@ public class LiftMovement : MonoBehaviour
         startPos = transform.parent.position; 
         targetPos = new Vector3(transform.position.x, transform.position.y - maximumDescent, transform.position.z);
     }
+    private void Update()
+    {
+        if (liftLeft)
+            Return();
+    }
     public void Descend()
     {
+        liftLeft = false;
         transform.parent.position = Vector3.MoveTowards(transform.parent.position, targetPos, Time.deltaTime);
     }
-
-    public void Return()
+    public void MarkLeft()
     {
-        float period = 0;
-        while(period != 5f)
+        liftLeft = true;
+    }
+    private void Return()
+    {
+        if(transform.parent.position == startPos) { return; }
+        else
         {
             transform.parent.position = Vector3.MoveTowards(transform.parent.position, startPos, Time.deltaTime);
-            period += Time.deltaTime;
         }
         
     }
